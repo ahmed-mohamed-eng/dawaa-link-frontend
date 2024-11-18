b74070861575580fbf43ef81c94ae9330e7f8f6e
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2024-11-18 02:28:31 +0200

    added the hero page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/header";
 import ActionHeader from "@/components/home/action-header";
+import HeroSection from "@/components/home/hero-section";
 import NavigationHeader from "@/components/home/navigation-header";
 
 export default function Home() {
@@ -10,7 +11,7 @@ export default function Home() {
       <NavigationHeader />
 
       <main className="w-full px-8 flex flex-col">
-
+        <HeroSection />
       </main>
     </div>
   );
---
+++ components/home/hero-section/index.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Image from "next/image";
+
+import React from "react";
+
+const HeroSection = () => {
+  //   const [currProductNumber, setCurrProductNumber] = useState(0);
+
+  return (
+    <div className="w-full px-16 py-6 grid grid-cols-2">
+      {/* Product Info */}
+      <div className="flex flex-col space-y-8 justify-center">
+        {/* Product Name */}
+        <div className="text-5xl font-bold text-primary w-fit">
+          <p>Canon</p>
+          <p className="mt-3">Camera</p>
+        </div>
+
+        {/* Hero Action */}
+        <div className="flex items-center space-x-6 w-fit">
+          <button className="bg-secondary px-8 py-4 rounded-2xl text-white font-bold">
+            Shop now
+          </button>
+          <button className="bg-white border border-primary px-8 py-4 rounded-2xl text-primary font-bold">
+            Shop now
+          </button>
+        </div>
+
+        {/* Navigation Buttons */}
+        <div className="flex items-center justify-center space-x-6">
+          <div className="w-4 h-4 bg-secondary rounded-full cursor-pointer" />
+          <div className="w-4 h-4 bg-white border border-secondary rounded-full cursor-pointer" />
+          <div className="w-4 h-4 bg-white border border-secondary rounded-full cursor-pointer" />
+        </div>
+      </div>
+
+      {/* Product Image */}
+      <div className="min-h-96 flex items-center justify-center py-20">
+        <div className="w-full h-96 relative">
+          <Image
+            alt="Featured Product"
+            src="/product.png"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+
+          <div className="absolute bottom-12 right-24 w-32 h-32 bg-secondary text-white font-bold text-2xl rounded-full flex flex-col items-center justify-center">
+            <p>Only</p>
+            <p>$89</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
