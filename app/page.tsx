3c39c2906c7479aa92e996a4f978c9d1a4bc73ed
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2024-11-18 02:00:07 +0200

    added header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/header";
 import ActionHeader from "@/components/action-header";
+import NavigationHeader from "@/components/navigation-header";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <ActionHeader />
+      <NavigationHeader />
     </div>
   );
 }
