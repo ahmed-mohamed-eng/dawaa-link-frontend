bc74a704023efa00df388259b0e68a97757d5c38
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2024-11-23 06:27:22 +0200

    added the storybook library to the project

---
+++ components/common-box/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CommonBox = () => {
+  return (
+    <div className="border-2 border-slate-200 rounded-lg px-16 py-12 col-span-2 row-span-2 flex flex-col space-y-4">CommonBox</div>
+  )
+}
+
+export default CommonBox;
