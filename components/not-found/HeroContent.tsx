c8594b9aa526d552e25e8cca37484c63eead2d1b
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2025-01-10 14:04:34 +0200

    added responsiveness to the not found page

--- components/not-found/HeroContent.tsx
+++ components/not-found/HeroContent.tsx
@@ -5,7 +5,7 @@ const HeroContent = () => {
   return (
     <div className="w-full flex flex-col relative">
       {/* Info */}
-      <div className="w-full h-[55rem] relative">
+      <div className="w-full h-96 lg:h-[55rem] relative">
         <Image
           alt="Hero"
           className="rounded-3xl -z-50"
@@ -14,17 +14,28 @@ const HeroContent = () => {
           style={{ objectFit: "cover" }}
         />
 
-        <div className="h-full w-full flex flex-col items-center justify-center px-48 font-bold space-y-8">
+        <div className="h-full w-full flex flex-col items-center justify-center px-4 lg:px-48 font-bold space-y-4 lg:space-y-8">
           <Image
+            className="hidden lg:block"
             alt="No page with the given name"
             src="/not-found.svg"
             width={825}
             height={360}
           />
-          <h1 className="w-full text-5xl text-center text-[#023E8A]">
+
+          <Image
+            className="block lg:hidden"
+            alt="No page with the given name"
+            src="/not-found.svg"
+            width={200}
+            height={125}
+          />
+
+          <h1 className="w-full text-xl lg:text-5xl text-center text-[#023E8A]">
             Opps ! The Page Not Found.
           </h1>
-          <p className="w-full text-lg text-center text-[#023E8A] font-thin">
+
+          <p className="w-full text-xs lg:text-lg text-center text-[#023E8A] font-thin">
             Proin non eros elementum, sagittis diam at, feugiat nunc. Ut velit
             arcu, posuere at neque quis, vestibulum vehicula dui. Praesent at
             felis ante. Cras sed ultricies risus. Nullam porta fermentum
@@ -33,7 +44,7 @@ const HeroContent = () => {
 
           <Link
             href="/"
-            className="bg-[#2A9BED] px-12 py-3 rounded-md font-black text-white text-xl capitalize"
+            className="bg-[#2A9BED] px-12 py-2 lg:py-3 rounded-md font-black text-white text-sm lg:text-xl capitalize"
           >
             back to home
           </Link>
