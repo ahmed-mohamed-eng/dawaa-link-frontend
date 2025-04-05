1dc0a3d61b71d87aba98ea7393ddf8ce648185bf
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2025-04-05 15:51:15 +0200

    added the open contact info

--- components/home/ContactInfo.tsx
+++ components/home/ContactInfo.tsx
@@ -4,10 +4,33 @@ import Image from "next/image";
 import { MouseEvent } from "react";
 import { v4 as uuid } from "uuid";
 import { useTranslations } from "next-intl";
-import toast, { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
+import { useRouter } from "@/i18n/routing";
 
 const ContactInfo = () => {
   const t = useTranslations("HomePage.ContactInfo");
+
+  const router = useRouter();
+
+  const onOpenLocationPage = () => {
+    router.push("/location");
+  };
+
+  const onOpenPhoneNumber = () => {
+    window.open("https://api.whatsapp.com/send?phone=+022589874533", "_blank");
+  };
+
+  const onOpenEmail = () => {
+    const email = {
+      to: "Contact@Medical.Com",
+      subject: "Inquiry",
+      body: "Hello Dawwa, I would like to inquire about your services. Best regards, [Your Name]",
+    };
+
+    const url = `https://mail.google.com/mail/?view=cm&fs=1&to=${email.to}&su=${email.subject}&body=${email.body}`;
+
+    window.open(url, "_blank");
+  };
 
   return (
     <div className="w-full rounded-t-2xl bg-[#081F48] text-white flex rtl:flex-row-reverse flex-col xl:flex-row items-center justify-center px-8 xl:px-0 py-8 xl:py-16 space-y-10 xl:space-y-0 xl:divide-x-2 xl:divide-[#707070]">
@@ -16,23 +39,29 @@ const ContactInfo = () => {
         <IconItem
           imageSrc="/icons/house-round.svg"
           contents={[t("address"), "813 Howard Street , Oswego NY 13126, USA"]}
+          onClick={onOpenLocationPage}
         />
 
         <IconItem
           imageSrc="/icons/phone-round.svg"
           contents={[t("phone"), "(+02) 258 987 4533", "(+02) 258 987 4533"]}
+          onClick={onOpenPhoneNumber}
         />
 
         <IconItem
           imageSrc="/icons/email-round.svg"
           contents={[t("email"), "Contact@Medical.Com"]}
+          onClick={onOpenEmail}
         />
 
         <Toaster position="bottom-center" />
       </div>
 
       {/* Physical Location */}
-      <div className="xl:pl-16 font-bold text-3xl flex flex-col items-center justify-start space-y-4">
+      <div
+        onClick={onOpenPhoneNumber}
+        className="xl:pl-16 font-bold text-3xl flex flex-col items-center justify-start space-y-4"
+      >
         <span className="text-sm">{t("description")}</span>
         <p>{t("title")}</p>
         <p>1200 - 256 - 48966</p>
@@ -46,23 +75,10 @@ export default ContactInfo;
 type IconItemProps = {
   imageSrc: string;
   contents: string[];
+  onClick?: (e: MouseEvent<HTMLParagraphElement>) => void;
 };
 
 function IconItem(props: IconItemProps) {
-  const onCopyContent = (e: MouseEvent<HTMLParagraphElement>) => {
-    const value = e.currentTarget.textContent;
-
-    if (!navigator || !navigator?.clipboard) {
-      return;
-    }
-
-    if (value) {
-      navigator.clipboard.writeText(value);
-
-      toast.success("Copied!");
-    }
-  };
-
   return (
     <div className="flex rtl:flex-row-reverse flex-col xl:flex-row xl:space-x-8 space-y-3 xl:space-y-0">
       <Image
@@ -87,7 +103,7 @@ function IconItem(props: IconItemProps) {
             <p
               key={uuid()}
               className="w-full overflow-hidden text-ellipsis cursor-pointer"
-              onClick={onCopyContent}
+              onClick={props.onClick}
             >
               {con}
             </p>
